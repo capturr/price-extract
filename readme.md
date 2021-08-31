73ac5cd7586bd8db397c3655b6c8e07f50ea9b9f
dopamyn <contact@gaetan-legac.fr>
2021-08-31 16:10:59 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,33 @@
+# JSON Deep Equal
+
+Check if json objects contains the same values (ignoring arrays order).
+
+[![npm](https://img.shields.io/npm/v/@dopamyn/price-extract)](https://www.npmjs.com/package/@dopamyn/price-extract)
+
+## Installation
+
+```bash
+npm install --save @dopamyn/price-extract
+```
+
+## Usage
+
+```typescript
+extractPrice( input: string, details?: boolean = false, debug?: boolean = false ): TPrice | number | null;
+```
+
+## Usage example
+
+```typescript
+import extractPrice from '@dopamyn/price-extract';
+
+console.log(
+    'Should be ok:',
+    ...['1 185,36 €', '$ 1,185,456.36', 'RUB 1185.36', '118536£'].map(v => price(v, false, true))
+);
+
+console.log(
+    'Should be null:',
+    ...['EUR 85,456.3', 'EUR 85,4556.34', '$ 1.185,456.36'].map(v => price(v, false, true))
+);
+```
---
+++ src/index.ts
@@ -0,0 +1,131 @@
+/*----------------------------------
+- CONST
+----------------------------------*/
+
+const currencies = [
+    "R$", "BRL",
+    "lei", "LEI", "Lei", "RON",
+    "$", "US$", "US dollars", "USD",
+    "£", "GBP",
+    "€", "Euro", "EUR",
+    "руб", "RUB",
+    "₪", "ILS",
+]
+
+const seps = ' ,.'
+const nb = '0123456789'
+
+const thousandsSize = 3;
+const decSize = 2;
+// Min size of amount: 4 ( 1,23 )
+const minAmountSize = decSize + 2;
+
+/*----------------------------------
+- TYPES
+----------------------------------*/
+
+type TAmount = { number: number, decsep?: string, grpsep?: string }
+type TPrice = TAmount & { currency: string }
+
+/*----------------------------------
+- METHODES
+----------------------------------*/
+
+export function extractAmount(str: string, debug: boolean = false): TAmount | null {
+
+    let amountStr = '';
+    let decsep: string | undefined;
+    let grpsep: string | undefined;
+    let grplength = 0;
+
+    const strlength = str.length;
+    for (let i = strlength - 1; i >= 0; i--) {
+
+        let c = str[i];
+
+        if (seps.includes(c)) {
+
+            // Decimals séparator
+            if (decsep === undefined || c === decsep) {
+
+                if (grplength !== decSize) {
+                    debug && console.log(`Bad decimals size:`, amountStr);
+                    return null;
+                }
+
+                decsep = c;
+                c = '.';
+
+                // Groups separator
+            } else {
+
+                // If thousands separator has already been defined, it must always be the same
+                if (grpsep !== undefined && grpsep !== c) {
+                    debug && console.log(`Inexpected separator: "${c}"`);
+                    break;
+                }
+
+                if (grplength !== thousandsSize) {
+                    debug && console.log(`Bad thousands size:`, amountStr);
+                    return null;
+                }
+
+                grpsep = c;
+                c = '';
+
+            }
+
+            grplength = 0;
+
+        } else if (nb.includes(c)) {
+
+            grplength++;
+
+        } else {
+            debug && console.log(`Inexpected caracter: "${c}"`);
+            break;
+        }
+
+        amountStr = c + amountStr;
+    }
+
+    debug && console.log(str, '=>', amountStr, { grpsep, decsep });
+
+    const number = parseFloat(amountStr);
+
+    return { number, decsep, grpsep }
+
+}
+
+export default function extractPrice(input: string, details: true, debug?: boolean): TPrice | null;
+export default function extractPrice(input: string, details?: false, debug?: boolean): number | null;
+export default function extractPrice(input: string, details: boolean = false, debug: boolean = false): TPrice | number | null {
+
+    let currency: { symb: string, index: number } | null = null;
+    for (const symb of currencies) {
+        const index = input.indexOf(symb);
+        if (index !== -1) {
+            currency = { symb, index };
+            break;
+        }
+    }
+
+    if (currency === null) return null;
+
+    let amount: TAmount | null = null;
+    if (currency.index >= minAmountSize) {
+        const strBefore = input.substring(0, currency.index).trim();
+        amount = extractAmount(strBefore, debug);
+    }
+
+    if (amount === null && currency.index <= input.length - minAmountSize) {
+
+        const strAfter = input.substring(currency.index + currency.symb.length).trim();
+        amount = extractAmount(strAfter, debug);
+    }
+
+    if (amount === null)
+        return null;
+
+    return details ? { ...amount, currency: currency.symb } : amount.number;
+}
